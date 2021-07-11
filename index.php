bc665e40804a1e94a593d023809db39d02bda9ee
Anne Sudari <anne.sudari@gmail.com>
2021-07-12 00:42:46 +0530

    home page initialized

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+require_once('components/header.php');
+?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12" id="index_carousel">
+            <div class="index_title_div">
+                <h1 >Chandrikawewa Jayanthi College</h1>
+                <h3>Motto here</h3>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <br>
+    <div class="row">
+        <div class="index_mission col-md-4">
+            <center><h4><b>MISSION</b></h4></center><br>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem distinctio mollitia repudiandae quos numquam voluptates quidem! Eligendi enim sapiente nemo sit sint, id quod fuga?!</p>
+        </div>
+        <div class="index_vision col-md-8">
+            <center><h4><b>VISION</b></h4></center><br>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos doloremque, perferendis officiis blanditiis consequuntur, modi suscipit laboriosam natus nobis voluptatibus sed amet corporis in accusamus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quo voluptatem error architecto saepe debitis suscipit sit labore exercitationem expedita?</p>
+        </div>
+    </div>
+</div>
+<br><br>
+<div class="container">
+    <div class="row">
+        <div class="index_principal_img col-md-4">
+            <img id="index_principal_img" src="./images/principal.png" alt="principal">
+        </div>
+        <div class="index_principal_msg col-md-8">
+            <h5>Principal's Message</h5><br>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, placeat. Consequuntur, commodi illo error nisi minima molestias similique, officia repellat perspiciatis qui corrupti soluta temporibus at. Fuga a nam fugiat sint aliquid perspiciatis quaerat voluptatibus mollitia ipsam nostrum cum, animi perferendis molestias quos expedita modi iusto blanditiis ex eveniet quod.</p>
+        </div>
+    </div>
+</div>
